d80f2d86f2b168405c68e0ea2d27a6ca0610f435
Elsie Waldheim Saury <waldheim.elsie@gmail.com>
2024-10-03 10:29:49 +0200

    finished with frontendtest

--- tests/testsuite.spec.ts
+++ tests/testsuite.spec.ts
@@ -11,7 +11,7 @@ import { generateUserData, generateRoomData, generateBillData, generateReservati
 test.describe('Frontend tests', () => {
  
   });
-    test("Test case 01, log in", async ({ page }) => {
+    test("Test case 01, log in and log out", async ({ page }) => {
       const loginPage = new LoginPage(page);
       const dashboardPage = new DashboardPage(page);
     
@@ -19,29 +19,41 @@ test.describe('Frontend tests', () => {
       await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`);
       await page.getByRole('button', { name: 'Login' }).click();
       await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+      await dashboardPage.performLogout();
+      await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
       
 });
 test("Test case 02, count clients", async ({ page }) => {
   const clientPage = new ClientPage(page);
   const clientData = generateClientData();
   const counterPage = new CounterPage(page);
+  const loginPage = new LoginPage(page);
+  const dashboardPage = new DashboardPage(page);
 
-  await page.goto('http://localhost:3000/login');
-  await expect(
-    page.getByRole("heading", { name: "Tester Hotel Overview" }),
-  ).toBeVisible();
+      await page.goto('http://localhost:3000/login');
+      await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`);
+      await page.getByRole('button', { name: 'Login' }).click();
+      await expect(page.getByRole("heading", { name: "Tester Hotel Overview" })).toBeVisible();
 
   // click on clients view
-  await page.locator("#app > div > div > div:nth-child(2) > a").click();
+      await page.locator("#app > div > div > div:nth-child(2) > a").click();
 
-  await counterPage.countClients();
+      await counterPage.countClients();
  
 });
 
 
 test.describe('Backend tests', () => {
-  test('has title', async ({ page }) => {
-    await page.goto('http://localhost:3000');
+  test('Test case 02 - Get all roomposts - v2', async ({ request }) => {
+    const clientsResponse = await request.get(`${process.env.BASE_URL}/api/rooms`, {
+      headers: xUserAuth
+    });
+    const getPosts = await apiHelper.getAllRoomPosts(request);
+    expect(clientsResponse.ok()).toBeTruthy();
+
+    // To see the data 
+    const roomData = await clientsResponse.json(); // Hämta JSON-data
+    console.log(roomData);
   
   });
   })
